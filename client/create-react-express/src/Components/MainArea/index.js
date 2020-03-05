da3cc8b481f5ea3f5b74c0f3f942dedb2abd183c
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-03-04 16:56:28 -0800

    Added components to test BlogCard flip function

---
+++ client/create-react-express/src/Components/MainArea/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../App.css';
+
+class MainArea extends Component {
+    render() {
+        return (
+            <div className="main-area">
+                <div className="blog-post">
+                    <p className="date">{new Date().toLocaleDateString()}</p>
+                    <p className="blog-content">
+                        Some sample text to demonstrate how these cards will work, including how they truncate long sentences.
+                </p>
+                    <p className="read-more">Hover to read more...</p>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default MainArea;
