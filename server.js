304d3569c793e77fa6730a1797de6b57931f6898
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-06-19 16:25:27 -0700

    modified home.js moviedb.js and server.js files

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const mongoose = require("mongoose");
 
 const routes = require("./routes");
 const app = express();
-
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
-
-// // Define middleware here
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
